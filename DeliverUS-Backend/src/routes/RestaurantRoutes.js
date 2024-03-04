25d97aecfe6865ccc284b6ea6457a9f7df0d44a1
Pablo Ferreria <pferreriahijon@gmail.com>
2024-03-04 20:27:28 +0100

    modified:   DeliverUS-Backend/src/routes/RestaurantRoutes.js 	modified:   package-lock.json

--- DeliverUS-Backend/src/routes/RestaurantRoutes.js
+++ DeliverUS-Backend/src/routes/RestaurantRoutes.js
@@ -1,7 +1,14 @@
 import RestaurantController from '../controllers/RestaurantController.js'
 
 const loadFileRoutes = function (app) {
-  // TODO: Include routes for restaurant described in the lab session README
-
+  app.route('/restaurant')
+    .post(isLoggedIn, hasRole('owner'), RestaurantController.create)
+    .get(RestaurantController.index)
+  app.route('/restaurant/:id')
+    .get(isLoggedIn, hasRole('owner'), RestaurantController.show)
+    .put(isLoggedIn, hasRole('owner'), RestaurantController.update)
+    .delete(isLoggedIn, hasRole('owner'), RestaurantController.destroy)
+  app.route('/restaurant/:id/description').get(isLoggedIn, hasRole('customer'), RestaurantController.index)
+  app.route('/restaurant/:id/menu').get(isLoggedIn, hasRole('customer'), RestaurantController.index)
 }
 export default loadFileRoutes
